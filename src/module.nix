46ed0cd69054cb0e6d3229cdd0a7b3da24c006ee
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-06 15:31:15 -0500

    Finish some module definitions

--- src/module.nix
+++ src/module.nix
@@ -6,8 +6,7 @@
   # Custom Nix-winget lib
   wingetLib = import ./lib.nix;
 
-  inherit (lib) mkEnableOption mkOption;
-  inherit (lib.types) listOf;
+  inherit (lib) mkEnableOption mkOption types;
 
   inherit (wingetLib.types) wingetPackage;
 in {
@@ -15,21 +14,37 @@ in {
     enable = mkEnableOption "Enable winget management";
 
     packages = mkOption {
-      type = listOf wingetPackage;
+      type = types.listOf wingetPackage;
       description = "Packages to install using winget";
       default = [];
     };
+
+    update-with-nixos =
+      mkEnableOption "Automatically update packages during a nixos-rebuild"
+      // {
+        default = true;
+      };
+
+    # TODO - implement this
+    unmanaged-packages-behavior = mkOption {
+      type = types.enum ["ignore" "uninstall"];
+      default = "ignore";
+      example = "uninstall";
+      description = ''
+        How to handle Winget packages not installed using Nix.
+        Setting this option to "ignore" ignores those packages
+        Setting this option to "uninstall" uninstalls those packages
+      '';
+    };
+
+    # TODO - implement this w/ Winget configuration files
+    configuration = mkEnableOption "Use a Winget configuration file to build the system.";
   };
 
   config.winget = let
     cfg = config.winget;
-
-    name = "nix-winget";
   in
     lib.mkIf cfg.enable {
-      systemd.services.${name} = {
-        inherit name;
-        description = "Manage Winget using Nix";
-      };
+      system.activationScripts.nix-winget = "";
     };
 }
